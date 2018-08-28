f6068feb3d12c5ecaa095c4f46479c09449f7635
natcecilio <tatafly2009@hotmail.com>
2018-08-28 11:06:45 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/tatafly/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.tatafly.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tatafly.cursomc.domain.Categoria;
+import com.tatafly.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) { 
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null); 
+		} 
+
+}
