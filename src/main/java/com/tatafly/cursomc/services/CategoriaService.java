f08fbc06774e9bc34ef1c04893e75d98dde20c55
natcecilio <tatafly2009@hotmail.com>
2018-09-25 09:18:28 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/tatafly/cursomc/services/CategoriaService.java
+++ src/main/java/com/tatafly/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.tatafly.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.tatafly.cursomc.domain.Categoria;
 import com.tatafly.cursomc.repositories.CategoriaRepository;
+import com.tatafly.cursomc.services.exceptions.DataIntegrityException;
 import com.tatafly.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+	}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 	
 }
